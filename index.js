ab097eb5857147546633ccc45fbc11994ad59bf5
tonymk0987 <k.tony.muriuki@gmail.com>
2024-03-23 21:13:25 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  function createFareMultiplier(multiplier) {
+    return function fareMultiplier(fare) {
+      return fare * multiplier;
+    };
+  }
+
+  function fareDoubler(fare) {
+    return fare * 2;
+  }
+
+  function fareTripler(fare) {
+    return fare * 3;
+  }
+
+  function selectDifferentDrivers(arrayOfDrivers, func) {
+    return func(arrayOfDrivers);
+  }
+
+  function selectDifferentDrivers(arrayOfDrivers, func) {
+    return func(arrayOfDrivers);
+  }
